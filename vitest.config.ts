89efb08ee7dcc47ffa7e78e75e575e661a04550a
Konrad Bysiek <konrad.bysiek@geeknauts.com>
2025-05-11 12:54:44 +0200

    chore(tests): Fixed vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./src/test/setup.ts'],
-    include: ['**/*.test.{ts,tsx}'],
+    include: ['./src/**/*.test.{ts,tsx}'],
     exclude: [
       'node_modules/',
       'src/test/',
